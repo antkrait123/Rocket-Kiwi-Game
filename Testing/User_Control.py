a64fc4408e9c0eb6ccabe4251aaa3f3690d939f9
Antony Crate <ACrate@stu.kerikerihigh.ac.nz>
2021-06-01 12:15:39 +1200

    Using kiwi sprite

--- Testing/User_Control.py
+++ Testing/User_Control.py
@@ -5,9 +5,9 @@ from pyglet.window.key import SPACE
 # Constants
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
-BALL_SPEED = 3
+Kiwi_speed = 5
 
-class Ball:
+class Kiwi:
     def __init__(self, position_x, change_x, position_y, change_y, radius, colour):
         self.position_x = position_x
         self.change_x = change_x
@@ -15,6 +15,7 @@ class Ball:
         self.change_y = change_y
         self.radius = radius
         self.colour = colour
+        
     
     def draw(self):
         arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.colour)
@@ -37,33 +38,34 @@ class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "User Controll")
         self.set_mouse_visible(False)
-        self.ball = Ball(300, 0, 500, 0, 30, RED)
+        self.kiwi = Kiwi(300, 0, 500, 0, 30, RED)
 
     def on_draw(self):
         arcade.set_background_color(SKY_BLUE)
         arcade.start_render()
         arcade.draw_lrtb_rectangle_filled(0,599, 200, 0, GREEN)
-        self.ball.draw()
+        self.kiwi.draw()
     
     def update(self, delta_time):
-        self.ball.update()
+        self.kiwi.update()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
-            self.ball.change_x = -BALL_SPEED
+            self.kiwi.change_x = -Kiwi_speed
         elif key == arcade.key.RIGHT:
-            self.ball.change_x = BALL_SPEED
+            self.kiwi.change_x = Kiwi_speed
         elif key == arcade.key.UP:
-            self.ball.change_y = BALL_SPEED
+            self.kiwi.change_y = Kiwi_speed
         elif key == arcade.key.DOWN:
-            self.ball.change_y = -BALL_SPEED
+            self.kiwi.change_y = -Kiwi_speed
+        
         
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.change_x = 0
+            self.kiwi.change_x = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.change_y = 0
-
+            self.kiwi.change_y = 0
+        
 
 def main():
     window = MyGame()
